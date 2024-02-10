37c485fc0a790df6389ad62dc1149caadd954a83
Mondal <subrata.b.mondal@capgemini.com>
2024-02-10 15:47:24 +0530

    fixed issue in main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,31 @@
 package com.driver;
 
 
-class classA {
-    public String meth() {
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        B b=new B();
+//        System.out.println(b.meth());
+//
+//    }
+//}
+class A
+{
+    public String meth()
+    {
         return "Invoking method from class A";
     }
 }
-
-class classB extends classA {
-    // Task 4: Override method meth of class A in class B
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
+class B extends A
+{
+    public String meth()
+    {
+        return  "Method is overridden in Extendend class B";
     }
-}
 
-public class Main {
     public static void main(String[] args) {
-        // Task 3: Create object of class B and call method meth of class A with it
-        classB obj = new classB();
-        System.out.println(obj.meth()); // Output: "Method is overridden in Extended class B" (Task 5)
+        B b=new B();
+        System.out.println(b.meth());
     }
 }
