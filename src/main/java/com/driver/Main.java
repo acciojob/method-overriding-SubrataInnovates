301dc7229b69483c3d46ce1f65661925210f2ff2
Mondal <subrata.b.mondal@capgemini.com>
2024-02-10 16:38:58 +0530

    put class A and B inside main method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,18 @@
-
 package com.driver;
-
-
 public class Main {
-    public static class A {
-        // Task 1: Method in class A returning a string
-        public String meth() {
+    public static class A{
+        public String meth(){
             return "Invoking method from class A";
         }
     }
-    public static class B extends A {
-        // Task 4: Override meth() method in class B
-        @Override
-        public String meth() {
-            return "Method is overridden in Extended class B";
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
         }
     }
-
     public static void main(String[] args) {
-        // Task 3: Create an object of class B and call meth() method of class A with it
-        B objB = new B();
-        
-        System.out.println(objB.meth()); // Task 5: Call overridden method from obj of class B
+        B obj = new B();
+        obj.meth();
+        System.out.println(obj.meth());
     }
 }
